29dcf0df4bec81d6c48da89c1ba9c303d0fc89bc
Neeraj1399 <neerajarvind582@gmail.com>
2026-02-25 15:00:10 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto text-center">
         <p className="text-sm">
-          &copy; 2025 P Neeraj Aravindhakshan. All rights reserved.
+          &copy; {currentYear} P Neeraj Aravindhakshan. All rights reserved.
         </p>
       </div>
     </footer>
